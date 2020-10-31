2e2b304bc885766f378d9aff9a601bc3e00a20cb
nlgamedev <nelsonliangamedev@hotmail.com>
2020-10-31 16:12:24 -0400

    seizure free feature added, rgb loop now only uses sin, optimzation module added but may be counter-intuitive. operation bugs fixed.

--- rgbif.py
+++ rgbif.py
@@ -4,27 +4,17 @@ import math
 from os import path
 import requests
 import arg_handler
+from pygifsicle import optimize
 
 my_arg_handler = arg_handler.rgbif_args()
 
-#arg_len = len(sys.argv)
-
 input_gif_path = my_arg_handler.input_path
-# input_gif_path = r"\Desktop\input.gif"
-# if arg_len >= 2:
-#     input_gif_path = sys.argv[1].encode('unicode-escape')
-#print(base_gif_path)
 
 output_gif_path = my_arg_handler.output_path
-# output_gif_path = r"\Desktop\output.gif"
-# if arg_len >= 3:
-#     output_gif_path = sys.argv[2].encode('unicode-escape')
-#print(output_gif_path)
 
 operator = my_arg_handler.operator
-# operator = "default"
-# if arg_len >= 4:
-#     operator = sys.argv[3].encode('ascii').decode('utf-8')
+
+minimum_frames = 24
 
 def isLocal(uri):
     if path.isfile(uri):
@@ -37,7 +27,6 @@ def scrape(imgURL):
     img_data = Blob(res.content)
     return img_data
 
-filterlist = ImageList()
 imgs = ImageList()
 single_gif = Image()
 
@@ -49,42 +38,58 @@ else:
     single_gif = Image(input_gif_path)
     imgs.readImages(input_gif_path)
 
-total_frames = len(imgs)
+
+
+def extendImageToMinimumFrames(img):
+    extendedImg = ImageList()
+    while len(extendedImg) < minimum_frames:
+        for frame in img:
+            extendedImg.append(frame)
+        #print(len(extendedImg))
+    return extendedImg
 
 def applyRGB(operator_string):
+    filterlist = ImageList()
+    total_frames = len(imgs)
+    #print(total_frames)
     for (i,frame) in enumerate(imgs):
-        percent = i/total_frames
-        r = max(1,int(65534*math.sin(percent*math.pi*2)))
-        g = max(1,int(65534*-math.sin(percent*math.pi*2)))
-        b = max(1,int(65534*-math.cos(percent*math.pi*2)))
-        #a = max(0,int(65534*math.sin(percent*math.pi*2)))
-        # offsetX = single_gif.columns() - frame.columns()
-        # offsetY = single_gif.rows() - frame.rows()
+        percent = float(i)/float(total_frames-1)
+        sR = 0.5*math.sin(percent*math.pi*2)+0.5
+        sG = -0.5*math.sin(percent*math.pi*2)+0.5
+        sB = 0.5*math.sin(percent*math.pi*4)+0.5
+        maxRGB = 65535
+        r = max(1,int(maxRGB*sR))
+        g = max(1,int(maxRGB*sB))
+        b = max(1,int(maxRGB*sG))
         c = Color(r,g,b)
+        
         filter = Image(Geometry(single_gif.columns(),single_gif.rows()),c)
-        if operator == "color":
+        
+        if operator_string == "color":
             frame.composite(filter, 0,0,CompositeOperator.ColorizeCompositeOp)
-        elif operator == "hue": 
+        elif operator_string == "hue": 
             frame.composite(filter, 0,0,CompositeOperator.HueCompositeOp)
-        elif operator == "overlay":    
+        elif operator_string == "overlay":    
             frame.composite(filter, 0,0,CompositeOperator.OverlayCompositeOp)
-        elif operator == "diff":
+        elif operator_string == "diff":
             frame.composite(filter, 0,0,CompositeOperator.DifferenceCompositeOp)
-        elif operator == "dissolve":    
+        elif operator_string == "dissolve":    
             frame.composite(filter, 0,0,CompositeOperator.DissolveCompositeOp)
-
-        frame.composite(filter, 0,0,CompositeOperator.ColorizeCompositeOp)
-            
+           
         frame.transparent(c)
         filterlist.append(frame)
+    return filterlist
+
+if len(imgs) < minimum_frames:
+    imgs = extendImageToMinimumFrames(imgs)
 
 if operator == "default":
-    applyRGB("color")
-    filterlist.writeImages(r"\Desktop\output_color.gif")
-    applyRGB("hue")
-    filterlist.writeImages(r"\Desktop\output_hue.gif")
+    applyRGB("color").writeImages(r"\Desktop\output_color.gif")
+    #optimize(r"\Desktop\output_color.gif")
+    applyRGB("hue").writeImages(r"\Desktop\output_hue.gif")
+    #optimize(r"\Desktop\output_hue.gif")
 else:
-    applyRGB(operator)
-    filterlist.writeImages(output_gif_path)
+    applyRGB(operator).writeImages(output_gif_path)
+    #optimize(output_gif_path)
 
 #https://cdn.discordapp.com/emojis/726328204373000272.gif?v=1
